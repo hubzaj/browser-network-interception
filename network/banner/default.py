72069b2cd2203414ccb3faedda2658bf84e0cd6e
Hubert Zajac <hubertzajac6@gmail.com>
2023-05-05 11:06:58 +0200

    adjustment

--- network/banner/default.py
+++ network/banner/default.py
@@ -13,12 +13,18 @@ class BannerAd(Enum):
     def get_ad_snippet(self) -> str:
         return self.value.safe_substitute(
             win_notification_snippet=self.__get_win_notification_snippet(),
-            click_tracking_snipper=self.__get_click_tracking_snippet()
+            click_tracking_snippet=self.__replace_click_url_macro(
+                self.__get_click_tracking_snippet()
+            )
         )
 
     @staticmethod
     def get_dsp_notification_url() -> str:
         return 'http://dsp.mock.url.org/win?partner=test_partner'
+
+    @staticmethod
+    def get_click_redirection_url() -> str:
+        return 'http://ssp.mock.url.org/record?event=click&r=https://www.bmw.pl'
 
     @staticmethod
     def get_ad_click_id_locator() -> str:
@@ -58,5 +64,13 @@ class BannerAd(Enum):
             """
         ).safe_substitute(
             id=cls.get_ad_click_id_locator(),
-            image_src=''
+            image_src='https://drive.google.com/file/d/1RJ6-oCLeMdaBbhxiH_k53AwagTtpwO4W/view?usp=share_link'
         )
+
+    @classmethod
+    def __replace_click_url_macro(cls, click_tracking_snippet: str) -> str:
+        """
+        Simulate SSP which is responsible for replacement of such macros
+        :return:
+        """
+        return click_tracking_snippet.replace('{clickurl}', cls.get_click_redirection_url())
