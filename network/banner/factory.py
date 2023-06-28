2e99186309735dae42ca5d23f742f8a664f2ad21
Hubert Zajac <hubertzajac6@gmail.com>
2023-06-28 19:53:22 +0200

    ad slot

--- network/banner/factory.py
+++ network/banner/factory.py
@@ -6,13 +6,17 @@ from .default import BannerAd
 __HTML_PREFIX: str = '<!DOCTYPE html><head><title></title></head><html><body>'
 __HTML_SUFFIX: str = '</body></html>'
 
+__HTML_AD_SLOT_PREFIX: str = '<div id="ad_slot">'
+__HTML_AD_SLOT_SUFFIX: str = '</div>'
+
 
 def create_html_page_with_ad(ad_html: str, directory: Path) -> Path:
-    page_with_ad_in_frame = ''.join([__HTML_PREFIX, ad_html, __HTML_SUFFIX])
+    ad_in_page_slot = ''.join([__HTML_AD_SLOT_PREFIX, ad_html, __HTML_AD_SLOT_SUFFIX])
+    page_with_ad_in_slot = ''.join([__HTML_PREFIX, ad_in_page_slot, __HTML_SUFFIX])
     return create_file(
         directory=directory,
         file_name='page_with_ad.html',
-        data=page_with_ad_in_frame
+        data=page_with_ad_in_slot
     )
 
 
