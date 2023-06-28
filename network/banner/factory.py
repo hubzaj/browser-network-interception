f23077a12fb7a5b72e36ae2a3b05c31a6303a6e3
Hubert Zajac <hubertzajac6@gmail.com>
2023-06-28 19:47:02 +0200

    iframe is old and unused approach - currently ads are displayed simply in div html elements

--- network/banner/factory.py
+++ network/banner/factory.py
@@ -6,13 +6,9 @@ from .default import BannerAd
 __HTML_PREFIX: str = '<!DOCTYPE html><head><title></title></head><html><body>'
 __HTML_SUFFIX: str = '</body></html>'
 
-__HTML_FRAME_PREFIX: str = ''
-__HTML_FRAME_SUFFIX: str = ''
-
 
 def create_html_page_with_ad(ad_html: str, directory: Path) -> Path:
-    ad_in_frame = ''.join([__HTML_FRAME_PREFIX, ad_html, __HTML_FRAME_SUFFIX])
-    page_with_ad_in_frame = ''.join([__HTML_PREFIX, ad_in_frame, __HTML_SUFFIX])
+    page_with_ad_in_frame = ''.join([__HTML_PREFIX, ad_html, __HTML_SUFFIX])
     return create_file(
         directory=directory,
         file_name='page_with_ad.html',
