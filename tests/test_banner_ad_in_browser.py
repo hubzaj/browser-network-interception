ea185f472dc5f16c04211073e5888f4c445cb447
Hubert Zajac <hubertzajac6@gmail.com>
2023-07-02 13:08:46 +0200

    [JIRA-11] request/response verification utils - apply

--- tests/test_banner_ad_in_browser.py
+++ tests/test_banner_ad_in_browser.py
@@ -1,6 +1,10 @@
+from http import HTTPStatus
+
 import pytest
 from selenium.webdriver.common.by import By
+from seleniumwire.request import Request
 
+from browser.utils.request import get_request_with_path
 from network.banner.default import BannerAd
 from network.keywords.browser import BrowserKeywords
 
@@ -13,29 +17,16 @@ class TestBannerAdInBrowser:
         ad: BannerAd = BannerAd.REGULAR_AD  # Product of the ad request sent to SSP
 
         # When
-        browser_keywords.display_ad_and_get_network_traffic(
+        requests: list[Request] = browser_keywords.display_ad_and_get_network_traffic(
             ad=ad,
             click_locator=(By.ID, ad.get_ad_click_id_locator()),
-            # Then
             requests_paths_to_wait=[
                 ad.get_dsp_notification_url(),
                 ad.get_click_redirection_url()
             ],
         )
 
-        # TODO: Add response verification (for the requests that are processed by us)
-
-    def test_example_2(self, browser_keywords: BrowserKeywords):
-        # Given
-        ad: BannerAd = BannerAd.REGULAR_AD  # Product of the ad request sent to SSP
-
-        # When
-        browser_keywords.display_ad_and_get_network_traffic(
-            ad=ad,
-            click_locator=(By.ID, ad.get_ad_click_id_locator()),
-            # Then
-            requests_paths_to_wait=[
-                ad.get_dsp_notification_url(),
-                ad.get_click_redirection_url()
-            ],
-        )
+        # Then
+        dsp_notification_request: Request = get_request_with_path(requests, path=ad.get_dsp_notification_url())
+        assert dsp_notification_request.response is not None
+        assert dsp_notification_request.response.status_code == HTTPStatus.OK
