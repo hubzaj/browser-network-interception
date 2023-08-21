87fd1c4552588adc9aa01ae4a1b3d04e05ffbc04
Hubert Zajac <hubertzajac6@gmail.com>
2023-08-21 15:22:27 +0200

    report

--- conftest.py
+++ conftest.py
@@ -1,26 +1,11 @@
 import os.path
-from dataclasses import dataclass
 from pathlib import Path
 
 import pytest
-import time
 from _pytest.fixtures import fixture
 
 from network.keywords.browser import BrowserKeywords
-
-
-@dataclass
-class PytestReportConfig:
-    """
-    [https://github.com/christiansandberg/pytest-reporter-html1]
-    """
-    dir_name = 'result'
-    report_name = 'pytest_report.html'
-    template = 'html1/index.html'
-
-    @staticmethod
-    def get_report_path(project_root_dir: str) -> str:
-        return os.path.join(project_root_dir, PytestReportConfig.dir_name, PytestReportConfig.report_name)
+from network.report.pytest_report import PytestReportConfig, pytest_publish_report
 
 
 def pytest_configure(config: pytest.Config):
@@ -36,7 +21,7 @@ def pytest_configure(config: pytest.Config):
     config.option.template = [PytestReportConfig.template]
 
 
-def pytest_sessionfinish(session, exitstatus):
+def pytest_sessionfinish(session: pytest.Session, exitstatus: pytest.ExitCode):
     """
     [https://docs.pytest.org/en/latest/reference/reference.html#pytest.hookspec.pytest_sessionfinish]
 
@@ -44,23 +29,9 @@ def pytest_sessionfinish(session, exitstatus):
     :param exitstatus:
     :return:
     """
-    __publish_pytest_report(session.fspath)
+    pytest_publish_report(str(session.fspath))
 
 
 @fixture
 def browser_keywords(tmp_path: Path) -> BrowserKeywords:
     yield BrowserKeywords(tmp_path)
-
-
-def __publish_pytest_report(project_root_dir: str) -> None:
-    pytest_report_path: str = PytestReportConfig.get_report_path(project_root_dir)
-
-    while not os.path.exists(pytest_report_path):
-        time.sleep(0.1)
-
-    if os.path.isfile(pytest_report_path):
-        # TODO: Publish test report to the QA related bucket (e.g. GCS, S3)
-        print('\n[TODO] Report has been published to [GCS_PATH] bucket within [GCP_PROJECT]\n')
-        pass
-    else:
-        raise FileNotFoundError(f'[{pytest_report_path}] is not a valid file path')
---
+++ network/report/pytest_report.py
@@ -0,0 +1,32 @@
+import os
+from dataclasses import dataclass
+
+import time
+
+
+@dataclass
+class PytestReportConfig:
+    """
+    [https://github.com/christiansandberg/pytest-reporter-html1]
+    """
+    dir_name = 'result'
+    report_name = 'pytest_report.html'
+    template = 'html1/index.html'
+
+    @staticmethod
+    def get_report_path(project_root_dir: str) -> str:
+        return os.path.join(project_root_dir, PytestReportConfig.dir_name, PytestReportConfig.report_name)
+
+
+def pytest_publish_report(project_root_dir: str) -> None:
+    pytest_report_path: str = PytestReportConfig.get_report_path(project_root_dir)
+
+    while not os.path.exists(pytest_report_path):
+        time.sleep(0.1)
+
+    if os.path.isfile(pytest_report_path):
+        # TODO: Publish test report to the QA related bucket (e.g. GCS, S3)
+        print('\n[TODO] Report has been published to [GCS_PATH] bucket within [GCP_PROJECT]\n')
+        pass
+    else:
+        raise FileNotFoundError(f'[{pytest_report_path}] is not a valid file path')
